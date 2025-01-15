4ed43d45a7871c0259d2b2675fd57b9aa35261a5
Keyra DeSouza <109808084+kdesouza2@users.noreply.github.com>
2025-01-15 04:03:52 +0000

    Starting todo's

--- 1-C-Refresher/stringfun.c
+++ 1-C-Refresher/stringfun.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 #define BUFFER_SZ 50
@@ -13,12 +14,6 @@ int  setup_buff(char *, char *, int);
 //prototypes for functions to handle required functionality
 int  count_words(char *, int, int);
 //add additional prototypes here
-
-
-int setup_buff(char *buff, char *user_str, int len){
-    //TODO: #4:  Implement the setup buff as per the directions
-    return 0; //for now just so the code compiles. 
-}
 
 void print_buff(char *buff, int len){
     printf("Buffer:  ");
@@ -33,11 +28,83 @@ void usage(char *exename){
 
 }
 
-int count_words(char *buff, int len, int str_len){
-    //YOU MUST IMPLEMENT
-    return 0;
+int setup_buff(char* buff, char* user_str, int len) {
+    // validate parameters
+    if (buff == NULL || user_str == NULL || len <= 0) {
+        printf("Error: Invalid Arguments.\n");
+        exit(3);
+    }
+
+    // temp pointers
+    char* input = user_str;
+    char* output = buff;
+
+    // length of output string
+    int output_len = 0;
+
+    bool last_char_was_space = false;
+
+    while (*input != '\0') {
+        if (*input == ' ' || *input == '\t') {
+            if (!last_char_was_space) {
+                if (output_len < len) {
+                    *output++ = ' ';
+                    output_len++;
+                } else {
+                    exit(1);
+                }
+            }
+
+            last_char_was_space = true;
+
+        } else {
+            if (output_len < len) {
+                *output++ = *input;
+                output_len++;
+            } else {
+                exit(1);
+            }
+            last_char_was_space = false;
+        }
+
+        input++;
+    }
+
+    // fill the remaining space in the buffer with '.'
+    while (output_len < len) {
+        *output++ = '.';
+        output_len++;
+    }
+
+    return (output_len - (len - BUFFER_SZ));
 }
 
+int is_space(char c) {
+    return (c == ' ' || c == '\t' || c == '\n');
+}
+
+int count_words(char *buff, int len, int str_len){
+    if (buff == NULL || str_len <= 0 || len <= 0) {
+        exit(1);
+    }
+
+    int count = 0;
+    bool inside_word = false;
+
+    for (int i = 0; i < str_len; i++) {
+        if (!is_space(buff[i])) {
+            if (!inside_word) {
+                count++;
+                inside_word = true;
+            }
+        } else {
+            inside_word = false;
+        }
+    }
+
+    return count;
+}
+ 
 //ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
 
 int main(int argc, char *argv[]){
@@ -48,8 +115,12 @@ int main(int argc, char *argv[]){
     int  rc;                //used for return codes
     int  user_str_len;      //length of user supplied string
 
-    //TODO:  #1. WHY IS THIS SAFE, aka what if arv[1] does not exist?
+    //TODO:  #1. WHY IS THIS SAFE, aka what if argv[1] does not exist?
     //      PLACE A COMMENT BLOCK HERE EXPLAINING
+    /*
+    The *argv[1] != '-' then that means that argv[1] is not a flag and therefore the program doesn't have the proper inputs.
+    Because of that we would want the program to exit(1), because it is a command line problem
+    */
     if ((argc < 2) || (*argv[1] != '-')){
         usage(argv[0]);
         exit(1);
@@ -67,6 +138,10 @@ int main(int argc, char *argv[]){
 
     //TODO:  #2 Document the purpose of the if statement below
     //      PLACE A COMMENT BLOCK HERE EXPLAINING
+    /*
+    If argc is less than 3 then that means that less than 3 arguments were passed in the command line
+    Since we have too little arguments we want the program to exit(1) because it is a command line problem
+    */
     if (argc < 3){
         usage(argv[0]);
         exit(1);
@@ -78,7 +153,11 @@ int main(int argc, char *argv[]){
     //          handle error if malloc fails by exiting with a 
     //          return code of 99
     // CODE GOES HERE FOR #3
-
+    buff = malloc(sizeof(char)*BUFFER_SZ);
+    if (buff == NULL) {
+        printf("Error: Memory Allocation Failed.\n");
+        exit(99);
+    }
 
     user_str_len = setup_buff(buff, input_string, BUFFER_SZ);     //see todos
     if (user_str_len < 0){
@@ -98,6 +177,14 @@ int main(int argc, char *argv[]){
 
         //TODO:  #5 Implement the other cases for 'r' and 'w' by extending
         //       the case statement options
+        case 'r':
+
+            break;
+        
+        case 'w':
+
+            break;
+
         default:
             usage(argv[0]);
             exit(1);
@@ -105,6 +192,7 @@ int main(int argc, char *argv[]){
 
     //TODO:  #6 Dont forget to free your buffer before exiting
     print_buff(buff,BUFFER_SZ);
+    free(buff);
     exit(0);
 }
 
@@ -115,3 +203,10 @@ int main(int argc, char *argv[]){
 //          the buff variable will have exactly 50 bytes?
 //  
 //          PLACE YOUR ANSWER HERE
+
+/*
+I think providing the pointer and the length is good practice, because it makes our jobs a lot easier
+Even though we know buff can have a length of 50, it's very probable that some of that space may not be used up
+Keeping track of the size of buff and not just its capacity allows us to focus on the useful information and not spend time trying 
+to figure out how much of buff we need to use
+*/
